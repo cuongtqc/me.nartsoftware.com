30e13c4fc0bfbf85cdcd5c5ce628a107b97661d9
Cuong Tran <quangcuong0808@gmail.com>
2024-01-08 00:40:06 +0700

    Update theme and tailwind layout

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full bg-gray-200 p-4 text-center">
+      <p className="text-sm text-gray-600">© 2024 Dash&apos;s Me Homepage. All rights reserved.</p>
+    </footer>
+  );
+};
---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,38 @@
+import {
+  Button,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar as FNavbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+
+export const Navbar = () => {
+  return (
+    <FNavbar fluid rounded className="w-full fixed border-b-2 shadow-teal-500/50">
+      <NavbarBrand href="https://flowbite-react.com">
+        <img
+          src="/favicon.ico"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Dash React
+        </span>
+      </NavbarBrand>
+      <div className="flex md:order-2">
+        <Button>Get started</Button>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink href="/">Home</NavbarLink>
+        <NavbarLink href="/about">About</NavbarLink>
+        <NavbarLink href="/services">Services</NavbarLink>
+      </NavbarCollapse>
+    </FNavbar>
+  );
+};
